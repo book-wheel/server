1431484107076c3abbe34260c97836896cc5a6be
soheesgit <thgml89567@smail.kognju.ac.kr>
2026-02-24 10:26:39 +0900

    feat: 그룹 대시보드 조회 기능 생성

--- src/main/java/com/bookwheel/server/schedule/Repository/RoundRepository.java
+++ src/main/java/com/bookwheel/server/schedule/Repository/RoundRepository.java
@@ -4,8 +4,10 @@ import com.bookwheel.server.schedule.entity.Round;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface RoundRepository extends JpaRepository<Round, String> {
     void deleteByGroup_GroupId(String groupId);
     List<Round> findByGroup_GroupIdOrderByRoundNumberAsc(String groupId);
+    Optional<Round> findByGroup_GroupIdAndRoundNumber(String groupId, Integer roundNumber);
 }
--- src/main/java/com/bookwheel/server/wheel/repository/WheelStateRepository.java
+++ src/main/java/com/bookwheel/server/wheel/repository/WheelStateRepository.java
@@ -6,9 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Optional;
 public interface WheelStateRepository extends JpaRepository<WheelState, String> {
-    Optional<WheelState> findByBookIdAndStatus(String roundId, String memberId);
-
-    List<WheelState> findByRoundIdAndOwnBook_OwnBookId(String roundId, String ownbookId);
-
-    Optional<WheelState> findFirstByRoundIdAndOwnBook_OwnBookId(String roundId, String ownbookId);
+    // 그룹 대시보드 조회 시, '내 책이 현재 라운드에서 어떤 멤버에게 가 있는지(myBookStep)'를 찾을 때 사용
+    Optional<WheelState> findFirstByRoundIdAndOwnBook_OwnbookId(String roundId, String ownbookId);
+    // 그룹 대시보드 조회 시, '내가 현재 라운드에서 어떤 책을 읽어야 하는지(myStep)'를 찾을 때 사용
+    Optional<WheelState> findByRoundIdAndMember_MemberId(String roundId, String memberId);
 }
