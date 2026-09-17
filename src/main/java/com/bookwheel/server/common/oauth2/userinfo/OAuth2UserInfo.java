97460678dfee4d3e60b17349f63bc7cec6768f56
hyeyeon25 <chohyeyeon1125@gmail.com>
2026-09-17 23:30:02 +0900

    fix: 소셜 로그인 기본 프로필 이미지 처리 수정 (#183)

--- src/main/java/com/bookwheel/server/common/oauth2/userinfo/OAuth2UserInfo.java
+++ src/main/java/com/bookwheel/server/common/oauth2/userinfo/OAuth2UserInfo.java
@@ -1,9 +1,6 @@
 package com.bookwheel.server.common.oauth2.userinfo;
 
-import java.util.Map;
-
 public interface OAuth2UserInfo {
     String getSocialId();     // 소셜 고유 ID (PK 대용)
     String getEmail();        // 이메일
-    String getProfileImage(); // 프로필 사진 URL
 }
